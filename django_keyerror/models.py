830c30c355deafec54e97bebfc241115f0c50767
Chris Lamb <chris@keyerror.com>
2016-03-17 07:57:05 +0800

    Support supporting Celery exceptions (Closes: #1)
    Signed-off-by: Chris Lamb <chris@keyerror.com>

--- django_keyerror/models.py
+++ django_keyerror/models.py
@@ -2,12 +2,15 @@ import sys
 import logging
 
 from django.http import Http404
+from django.conf import settings
 from django.core.signals import got_request_exception
 
 from . import app_settings
-from .error import DjangoError
+from .error import DjangoError, QueueError
 
 logger = logging.getLogger(__name__)
+
+## Django ####################################################################
 
 def report_exception(sender, request, **kwargs):
     if not app_settings.ENABLED:
@@ -24,3 +27,19 @@ def report_exception(sender, request, **kwargs):
         logger.exception("Exception whilst reporting error to keyerror.com")
 
 got_request_exception.connect(report_exception)
+
+## Celery ####################################################################
+
+if 'djcelery' in settings.INSTALLED_APPS:
+    from celery import signals
+
+    def report_task_failure(einfo, traceback, **kwargs):
+        if not app_settings.ENABLED:
+            return
+
+        try:
+            QueueError(einfo.type, einfo.exception, traceback).send()
+        except Exception:
+            logger.exception("Exception whilst reporting error to keyerror.com")
+
+    signals.task_failure.connect(report_task_failure)
