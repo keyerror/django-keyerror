7e086ec1be18f06099f8c42d0729a95249601e5a
Chris Lamb <chris@keyerror.com>
2016-03-16 12:45:06 +0000

    Releasing version 2.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-keyerror',
-    version='2.0.1',
+    version='2.1.0',
     description='KeyError.com Django client',
 
     url='https://keyerror.com/docs/django',
