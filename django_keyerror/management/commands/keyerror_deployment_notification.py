43516fa9ccced2e064c1dd3f682b62470c76c1e2
Chris Lamb <chris@keyerror.com>
2016-10-22 17:27:43 +0200

    Use BaseCommand.add_arguments, not *args.
    Signed-off-by: Chris Lamb <chris@keyerror.com>

--- django_keyerror/management/commands/keyerror_deployment_notification.py
+++ django_keyerror/management/commands/keyerror_deployment_notification.py
@@ -10,20 +10,19 @@ class Command(BaseCommand):
     args = '[options] "<deployment title>" "<deployment url>"'
     requires_model_validation = False
 
+    def add_arguments(self, parser):
+        parser.add_argument('title')
+        parser.add_argument('url')
+
     def handle(self, *args, **options):
         if not app_settings.ENABLED:
             raise CommandError("KeyError is not enabled; exiting.")
 
         post_url = app_settings.URL % '/deployments'
 
-        try:
-            title, url = args
-        except ValueError:
-            raise CommandError("Invalid arguments: %s" % self.args)
-
         req = urllib2.Request(post_url, urllib.urlencode({
-            'url': url,
-            'title': title,
+            'url': options['url'],
+            'title': options['title'],
         }), {
             'X-API-Key': app_settings.SECRET_KEY,
         })
