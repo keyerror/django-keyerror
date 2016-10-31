5b0821e90c485213aebd3833173765860517dadc
Chris Lamb <chris@keyerror.com>
2016-10-31 10:17:25 +0000

    Rework app_setings
    Signed-off-by: Chris Lamb <chris@keyerror.com>

--- django_keyerror/app_settings.py
+++ django_keyerror/app_settings.py
@@ -2,6 +2,8 @@ from django.core import mail
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 
+TEMPLATE = 'KEYERROR_%s'
+
 class NOT_PROVIDED:
     pass
 
@@ -10,40 +12,45 @@ def setting(suffix, default=NOT_PROVIDED):
     # @override_settings works.
     @property
     def fn(self):
-        key = 'KEYERROR_%s' % suffix
+        key = TEMPLATE % suffix
 
         try:
             if default is NOT_PROVIDED:
                 return getattr(settings, key)
         except AttributeError:
             raise ImproperlyConfigured(
-                "Missing required setting: {}".format(key),
+                "Missing required setting: {}".format(key)
             )
 
         return getattr(settings, key, default)
+
     return fn
 
 class AppSettings(object):
     SECRET_KEY = setting('SECRET_KEY')
+
     USER_INFO_CALLBACK = setting(
         'USER_INFO_CALLBACK',
         'django_keyerror.utils.get_user_info',
     )
 
     URL = setting('URL', 'http://api.keyerror.com/v1%s')
+    TIMEOUT = setting('TIMEOUT', 1)
+
     HOST = setting('HOST', 'api.keyerror.com')
     PORT = setting('PORT', 2930)
-    TIMEOUT = setting('TIMEOUT', 1)
 
     @property
     def ENABLED(self):
-        if hasattr(mail, 'outbox'):
-            False
-
+        # Always respect ENABLED if specified
         try:
-            return setting('ENABLED')
-        except ImproperlyConfigured:
-            # If we haven't overridden this, fallback to settings.DEBUG
+            return getattr(settings, TEMPLATE % 'ENABLED')
+        except AttributeError:
+            # If we haven't overridden this, first check we aren't running tests
+            if hasattr(mail, 'outbox'):
+                return False
+
+            # .. then assume that if we are debugging, we don't want to run i
             return not settings.DEBUG
 
 app_settings = AppSettings()
