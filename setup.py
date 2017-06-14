33da680e90a0850479765fa0326835a2f20f8219
Chris Lamb <chris@keyerror.com>
2017-06-14 08:40:59 +0800

    Releasing version 3.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-keyerror',
-    version='3.1.0',
+    version='3.1.1',
     description='KeyError.com Django client',
 
     url='https://keyerror.com/docs/django',
